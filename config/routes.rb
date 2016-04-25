166ada612a76966e6f8f2679a90a95b8ec832b77
Sal Espinosa <sespinos@gmail.com>
2016-04-24 18:15:15 -0600

    Reorganized for clarity

--- config/routes.rb
+++ config/routes.rb
@@ -2,25 +2,25 @@ Rails.application.routes.draw do
   root "homes#show"
   resources :homes, only: [:show]
 
+
+  resources :users, only: [:new, :create]
   resources :coders, only: [:index, :show]
+  resources :teams, only: [:create, :index, :destroy]
+  resources :contracts, only: [:create, :show]
+
 
   namespace :admin do
-    get "/dashboard", to: "users#show", as: :dashboard
     resources :users, only: [:edit, :update]
+    get "/dashboard", to: "users#show", as: :dashboard
   end
+
 
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-
-  resources :users, only: [:new, :create]
   get "/dashboard", to: "users#show", as: :dashboard
-
-  resources :contracts, only: [:create, :show]
-
   get "/history", to: "contracts#index"
 
-  resources :teams, only: [:create, :index, :destroy]
 
   get "/:slug", to: "categories#show", as: :category
 end
